7c536bfb565e65d9369748e555062c507cbbb646
Fabian Schaffenrath <fabiansc@edu.aau.at>
2021-07-30 12:53:59 +0200

    routing test

--- routes/fee.js
+++ routes/fee.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-// TODO
+router.get('',function (req,res,next){
+    console.log("First");
+    //next();
+});
+
+router.get('',function (req,res,next){
+    console.log("Second");
+    return res.status(200).json({message:'Hello'});
+});
 
 module.exports = router;
