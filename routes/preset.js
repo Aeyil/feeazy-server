d80c8c00acca4c9f2a240cdee43beecad688d066
Fabian Schaffenrath <fabiansc@edu.aau.at>
2021-07-30 08:51:28 +0200

    testing promise chaining

--- routes/preset.js
+++ routes/preset.js
@@ -12,7 +12,6 @@ router.get('',function (req,res){
     db.getClient()
         .then((client) =>{
             console.log("TEST1");
-            res.status(200).json({ message: "part1 complete"});
             return "Hello";
         },(error) => {
             console.log("TESTERR");
